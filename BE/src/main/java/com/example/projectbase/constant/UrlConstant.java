c038246facd25a6e0f071162655b5c4d21c987a5
LeMinHieu <lminhhieu2507@gmail.com>
2025-06-21 17:54:24 +0700

    (Ref #5) Fix: changed processing video and audio

--- BE/src/main/java/com/example/projectbase/constant/UrlConstant.java
+++ BE/src/main/java/com/example/projectbase/constant/UrlConstant.java
@@ -31,7 +31,21 @@ public class UrlConstant {
     public static final String GET_USERS = PRE_FIX;
     public static final String GET_USER = PRE_FIX + "/{userId}";
     public static final String GET_CURRENT_USER = PRE_FIX + "/current";
+
     private User() {
     }
   }
+
+  public static class Media {
+    private static final String PRE_FIX = "/media";
+    public static final String GET_MEDIA_BY_RESOURCE_TYPE = PRE_FIX + "/";
+    public static final String GET_MEDIA_BY_PUBLIC_ID = PRE_FIX + "/{publicId}";
+    public static final String GET_AUDIO_BY_TITLE_OR_CATEGORY_OR_SINGER = PRE_FIX + "/audio";
+    public static final String UPLOAD_MEDIA_VIDEO = PRE_FIX + "/upload/video";
+    public static final String UPLOAD_MEDIA_IMAGE = PRE_FIX + "/upload/image";
+    public static final String UPLOAD_MEDIA_AUDIO = PRE_FIX + "/upload/audio";
+    public static final String UPLOAD_MULTI_MEDIA_IMAGE = PRE_FIX + "/upload/multi/image";
+    public static final String DELETE_MEDIA = PRE_FIX + "/delete";
+  }
+
 }
