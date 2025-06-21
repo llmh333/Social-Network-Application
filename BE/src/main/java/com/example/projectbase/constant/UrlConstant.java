5c9ecbe6cd6413370e126393bf619fd7fd765a8c
LeMinHieu <lminhhieu2507@gmail.com>
2025-06-21 09:46:21 +0700

    (Ref #6) feature: Implement login with facebook

--- BE/src/main/java/com/example/projectbase/constant/UrlConstant.java
+++ BE/src/main/java/com/example/projectbase/constant/UrlConstant.java
@@ -6,8 +6,9 @@ public class UrlConstant {
     private static final String PRE_FIX = "/auth";
 
     public static final String LOGIN = PRE_FIX + "/login";
+    public static final String LOGIN_GOOGLE = PRE_FIX + "/login/google";
+    public static final String LOGIN_FACEBOOK = PRE_FIX + "/login/facebook";
     public static final String LOGOUT = PRE_FIX + "/logout";
-    public static final String OAUTH2_LOGIN = PRE_FIX + "/oauth2-login";
     public static final String REFRESH_TOKEN = PRE_FIX + "/refresh-token";
     public static final String ME = PRE_FIX + "/me";
     public static final String UPLOAD_PROFILE_PICTURE = PRE_FIX + "/upload-profile-picture";
@@ -15,6 +16,13 @@ public class UrlConstant {
 
     private Auth() {
     }
+  }
+
+  public static class OAUTH2_INFO {
+    public static final String PRE_FIX = "/oauth2";
+    public static final String REDIRECT_OAUTH2_GOOGLE =  PRE_FIX + "/authorization/google";
+    public static final String REDIRECT_OAUTH2_FACEBOOK = PRE_FIX + "/authorization/facebook";
+    public static final String OAUTH2_TOKEN_INFO = PRE_FIX + "/info/token";
   }
 
   public static class User {
